af0b9d263f88f9c1a0abbfa3a9935fbc350f382b
Saurabh Goel <saurabh.goel@allonblock.com>
2019-04-25 14:54:19 +0530

    changes in env props

--- ios-release.sh
+++ ios-release.sh
@@ -2,7 +2,7 @@
 
 echo "------Building release for IOS--------"
 touch ios/MyFMSApp/Info.plist
-cp firebase-prod/GoogleService-Info.plist ios/GoogleService-Info.plist
+cp firebase-dev/GoogleService-Info.plist ios/GoogleService-Info.plist
 
 ENVFILE=.env.production xcodebuild \
     -workspace ./ios/MyFMSApp.xcworkspace \
@@ -12,6 +12,6 @@ ENVFILE=.env.production xcodebuild \
     -archivePath ./iOS/release/MyFMSApp.xcarchive
 
 echo "---------DONE-----------"
-echo "Copying back the dev firebase file"
-cp firebase-dev/GoogleService-Info.plist ios/GoogleService-Info.plist
-echo "--------Copy Done--------"
+#echo "Copying back the dev firebase file"
+#cp firebase-dev/GoogleService-Info.plist ios/GoogleService-Info.plist
+#echo "--------Copy Done--------"
