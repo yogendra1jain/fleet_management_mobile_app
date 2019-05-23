d36bb6d6ef1c7ce954b5deab08e1a3168b4dd56c
Saurabh Goel <saurabh.goel@allonblock.com>
2019-05-23 11:36:57 +0530

    IOS build with upgraded versions

--- ios/MyFMSApp/AppDelegate.m
+++ ios/MyFMSApp/AppDelegate.m
@@ -9,7 +9,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import "SplashScreen.h"
+#import "RNSplashScreen.h"
 
 @implementation AppDelegate
 
@@ -32,7 +32,7 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
 
-  [SplashScreen show];
+  [RNSplashScreen show];
   return YES;
 }
 
